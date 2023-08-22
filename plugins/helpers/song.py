c6657472dcf85baeed378f4f5089a1bbd8ec60ae
priyakapp <84661064+priyakapp@users.noreply.github.com>
2023-08-23 04:51:09 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› **\n**ᴘᴏᴡᴇʀᴇᴅ ʙʏ -›› [ᴍᴏᴠɪᴇꜱ ᴜɴʟᴏᴀᴅᴇᴅ](https://t.me/MOVIES_UNLOADED_backup)**\n@ᴍᴏᴠɪᴇꜱ ᴜɴʟᴏᴀᴅᴇᴅ**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› **\n**ᴘᴏᴡᴇʀᴇᴅ ʙʏ -›› [Pixtar XD](https://t.me/+NVUJZ3u6Mak3NWJl)**\n@Pixtar XD**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
